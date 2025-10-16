3bdffe7830cbc95be7695f0767e2619d5d04281f
danang0202 <wisnudanang86@gmail.com>
2025-10-16 19:47:07 +0700

    fix: basename router

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ const AppContent: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    <Router basename="/webmon">
+    <Router>
       <AuthProvider>
         <AppContent />
       </AuthProvider>
